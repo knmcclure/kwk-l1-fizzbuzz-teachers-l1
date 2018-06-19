49fe62878cab4b19d97e7cf9ed169ce4b66d0617
Kyla McClure <kyla.mcclure.16@tfacorps.org>
2018-06-19 18:19:51 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(int)
-  if int%3==0
+  if 
+    int%3==0
     puts "Fizzzzzzzzzzzzzzzzzz"
   elsif int%5 == 0
     puts "Buzzzzzzzzzzzzzzz"
-  elsif int%15 == 0
-    puts "FizzzzzzzzzzzBuzzzzzzzzzzz"
+  elsif 
   end
 end
 
